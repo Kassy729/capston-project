4b4745aa45c21e7839dfd2fe30747f6fa19e1036
Kassy729 <hamtol98@gmail.com>
2022-03-16 02:11:02 +0900

    수정

--- backend/app/Http/Middleware/Authenticate.php
+++ backend/app/Http/Middleware/Authenticate.php
@@ -24,7 +24,6 @@ class Authenticate extends Middleware
     public function handle($request, Closure $next, ...$guards)
     {
         if ($login_token = $request->cookie('login_token')) {
-            return $login_token;
             $request->headers->set('Authorization', 'Bearer ' . $login_token);
         } else {
             return response([
